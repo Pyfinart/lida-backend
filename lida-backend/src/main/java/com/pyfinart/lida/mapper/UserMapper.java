34a0b297fa80d111f1378b88ae2cce237dc40833
pyfinart <mypyfinart@gmail.com>
2024-06-14 11:03:57 +0800

    first commit

---
+++ lida-backend/src/main/java/com/pyfinart/lida/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.pyfinart.lida.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pyfinart.lida.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
